75e7ceb0367000a7dcd5c617f3e70c775d2c09c5
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-10-24 10:57:42 +0200

    Exercise 002 Arithmetic operations

---
+++ java/002-Arithmetic.java
@@ -0,0 +1,11 @@
+// 002 Arithmetic operations in Java
+
+public class Main {
+    public static void main(String[] details) {
+        System.out.println("7+2=" + (7+2));
+        System.out.println("7-2=" + (7-2));
+        System.out.println("7*2=" + (7*2));
+        System.out.println("7/2=" + (7/2));
+        System.out.println("7%2=" + (7%2));
+    }
+}
