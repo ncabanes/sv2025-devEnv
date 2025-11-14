7c99449b1535b601604f76a9ce78bae46404b8a3
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-11-14 20:51:13 +0100

    Java 009 - Calculator + loop (while+do-while)

---
+++ java/009b-Calculator-doWhileLoop.java
@@ -0,0 +1,69 @@
+/*
+Ask the user for a integer number, an operator and another integer 
+number, and display the result of such operation, like in this example:
+
+First number? 5
+Operator? +
+Second number? 7
+
+5 + 7 = 12
+
+The program must repeat until the user presses Enter instead of typing 
+in a first number
+
+Version 2: "do-while" instead of "while"
+
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String aTxt;
+       
+        do
+        {
+            System.out.print("Enter the first number: ");
+            aTxt = sc.nextLine();
+            
+            if (! aTxt.equals(""))
+            {
+                int a = Integer.parseInt(aTxt);
+                
+                System.out.print("Enter the operation: ");
+                String operator = sc.nextLine();
+                
+                System.out.print("Enter the second number: ");
+                int b = sc.nextInt();
+                sc.nextLine();
+                
+                switch(operator) {
+                    case "+":
+                        System.out.println(a+"+"+b+"="+(a+b));
+                        break;
+
+                    case "-":
+                        System.out.println(a+"-"+b+"="+(a-b));
+                        break;
+                    
+                    case "*":
+                    case "x":
+                    case "·":
+                        System.out.println(a+operator+b+"="+(a*b));
+                        break;
+                    
+                    case "/":
+                        System.out.println(a+"/"+b+"="+((float)a/b));
+                        break;
+                    
+                    default:
+                        System.out.println("Unknown operator");
+                }
+            }
+        }
+        while (! aTxt.equals(""));
+    }
+}
+
