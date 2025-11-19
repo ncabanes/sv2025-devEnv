77b3fb903d8ac7e2e3cf7d6c027b5fca5ff9188b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-11-19 20:55:05 +0100

    Java 010 - Day of the week (if+switch)

---
+++ java/010-DayOfTheWeek.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the day number (0 to 7): ");
+        int day = sc.nextInt();
+        
+        if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else if (day == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Wrong day");
+        } 
+        
+        switch (day)
+        {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
+            case 7: System.out.println("Sunday"); break;
+            default: System.out.println("Wrong day"); break;
+        }
+    }
+}
+
